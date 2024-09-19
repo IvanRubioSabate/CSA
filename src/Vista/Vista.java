0cb5d01a2682642cabcc705597c8f41525af98f8
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-09-19 09:44:52 +0200

    cards layout test

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -2,11 +2,41 @@ package Vista;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Vista {
 
     private static MyFrame frame = new MyFrame();
+    private static JPanel cards;
+    private static CardLayout cl;
     public static void main(String[] args) throws Exception {
         
+        cl = new CardLayout();
+
+        cards = new JPanel();
+        cards.setLayout(cl);
+        
+
+        JPanel panel1 = new JPanel();
+        JButton text1 = new JButton("Hola");
+        text1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                cl.show(cards, "Panel2");
+            }
+        });
+        panel1.add(text1);
+
+        JPanel panel2 = new JPanel();
+        JButton text2 = new JButton("Adios");
+        panel2.add(text2);
+
+        cards.add(panel1, "Panel1");
+        cards.add(panel2, "Panel2");
+
+        cl.show(cards, "Panel1");
+
+        frame.setContentPane(cards);
+        frame.setVisible(true);
     }
 }
