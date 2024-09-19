95f98bdc3d9990d79aa6d67ad8fad5fcac6cd51a
AbadockC <148381552+GCantanhede@users.noreply.github.com>
2024-09-19 09:08:49 +0200

    DB COnection + librari import

---
+++ src/Modelo/ConectionDB.java
@@ -0,0 +1,31 @@
+package Modelo;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConectionDB {
+    
+    private final String url = "jdbc:mysql://localhost:3306/csabd";
+    private final String usuari = "root";
+    private final String password = "patata";
+    
+    public ConectionDB(){
+    }
+    
+    public java.sql.Connection getConexio(){
+        try {
+            return DriverManager.getConnection(url, usuari, password);
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public  void closeConexio(java.sql.Connection conn){
+        try {
+            conn.close();
+            System.out.println("Conexió tancada");
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -1,6 +1,30 @@
 package Vista;
+
+import Modelo.ConectionDB;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Vista {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ConectionDB conec = new ConectionDB();
+        
+        Connection conn = conec.getConexio();
+        
+        try {
+            Statement sentencia = conn.createStatement();
+            
+            sentencia.execute("INSERT INTO usuarios (usuario_usuario, usuario_contrasena, usuario_rol, usuario_nombre, usuario_sexo, usuario_edad, usuario_direcion) VALUES ('Guilherme', 'patata', 'ADMIN','Guilherme' ,'hombre', '19', 'Vidreres')");
+            
+            ResultSet rs = sentencia.executeQuery("SELECT * FROM usuarios");
+            
+            while (rs.next()) {
+                System.out.println(rs.getString("usuario_usuario") + " " + rs.getString("usuario_rol"));
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
