cc934973ad21b18641975b7d78c20d2619e8a0fb
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-09-19 08:56:41 +0200

    default frame

--- src/Vista/MyFrame.java
+++ src/Vista/MyFrame.java
@@ -7,6 +7,7 @@ public class MyFrame extends JFrame {
     public MyFrame() {
         setTitle("hola");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 800, 800);
         setVisible(true);
         setResizable(true);
     }
