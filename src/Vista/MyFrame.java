920b780a56541e6137e40e4b2befa0d5fc633faa
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-09-19 09:49:40 +0200

    fixed MyFrame

--- src/Vista/MyFrame.java
+++ src/Vista/MyFrame.java
@@ -7,9 +7,8 @@ public class MyFrame extends JFrame {
 
     public MyFrame() {
         // Crear el marco principal
-        JFrame frame = new JFrame();
-        setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
-        frame.setBounds(0, 0, 400, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(0, 0, 400, 500);
     }
 
 
