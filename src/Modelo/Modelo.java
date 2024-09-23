3e052d4e67637643e843d3f6ac032c126a971683
AbadockC <148381552+GCantanhede@users.noreply.github.com>
2024-09-23 13:14:58 +0200

    Login all working

--- src/Modelo/Modelo.java
+++ src/Modelo/Modelo.java
@@ -5,8 +5,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Modelo {
-
-    private static User activeUser;
+    
     public static void main(String[] args) {
         System.out.println(createUser("Ivan", "patata", "ADMIN", "Ivan", "hombre", 19, "Vidreres"));
     }
