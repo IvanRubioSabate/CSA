931fad74332bb20a355f4f2bec121a7f2bb52c40
IvanRubioSabate <ivan.rubio@institutvidreres.cat>
2024-09-29 16:33:55 +0200

    addUsuario fet amb sobrecarrega per que afegeixi l'usuari de forma different segons l'objecte que se li passi

--- src/Modelo/Usuario.java
+++ src/Modelo/Usuario.java
@@ -43,70 +43,81 @@ public class Usuario {
         }
     }
 
-    public static boolean addUsuario(Usuario usuario, String rol, String contrasena) throws SQLException {
+    public static boolean addUsuario(Usuario usuario, String contrasena) throws SQLException {
 
+        if (!userExists(usuario)) {
+            return addToTableUsuarios(usuario, "ADMIN", contrasena);
+        }
+        return false;
+    }
+
+    public static boolean addUsuario(Mecanico meca, String contrasena) throws SQLException {
         ConectionDB conec = new ConectionDB();
         Connection conn = conec.getConexio();
 
-        Statement sentencia = conn.createStatement();
-        ResultSet rs = sentencia.executeQuery("select usuario_usuario from usuarios where usuario_usuario = '" + usuario.getUsuario() + "';");
+        if (!userExists(meca)) {
+            try {
+                addToTableUsuarios(meca, "MECA", contrasena);
+                PreparedStatement stmt = conn.prepareStatement("INSERT INTO mecanico"+
+                        "(mecanico_id, mecanico_salario, mecanico_id_taller, mecanico_anos_exp) "+
+                        "VALUES (?,?,?,?);");
 
-        if (rs.next()) {
-            return false;
-        }
+                stmt.setString(1, meca.getUsuario());
+                stmt.setInt(2, meca.getSalario());
+                stmt.setInt(3, meca.getIDTaller());
+                stmt.setInt(4, meca.getAnosExp());
+                stmt.executeUpdate();
 
-        boolean userAdded = addToTableUsuarios(usuario, rol, contrasena);
-
-        if (userAdded) {
-            switch (rol.toUpperCase()) {
-                case "ADMIN":
-                    return true;
-                case "MECA":
-                    if (usuario instanceof Mecanico meca) {
-                        try {
-                            PreparedStatement stmt = conn.prepareStatement("INSERT INTO mecanico"+
-                                    "(mecanico_id, mecanico_salario, mecanico_id_taller, mecanico_anos_exp) "+
-                                    "VALUES (?,?,?,?);");
-
-                            stmt.setString(1, usuario.getUsuario());
-                            stmt.setInt(2, meca.getSalario());
-                            stmt.setInt(3, meca.getIDTaller());
-                            stmt.setInt(4, meca.getAnosExp());
-                            stmt.executeUpdate();
-
-                            ConectionDB.closeConexio(conn);
-                            return true;
-                        } catch (SQLException e) {
-                            System.out.println("error " + e.getMessage());
-                        }
-                    }
-                    break;
-
-                case "ASTRONAUTA":
-                    if (usuario instanceof Astronauta astr) {
-                        try {
-                            PreparedStatement stmt = conn.prepareStatement("INSERT INTO astronauta"+
-                                    "(astronauta_id, astrouauta_data_1r_vuelo, astronauta_misiones_ok, astronauta_misiones_ko, astronauta_rango_militar) "+
-                                    "VALUES (?,?,?,?,?);");
-
-                            stmt.setString(1, usuario.getUsuario());
-                            stmt.setDate(2, astr.getFechaPrimerVuelo());
-                            stmt.setInt(3, astr.getMisionesOK());
-                            stmt.setInt(4, astr.getMisionesKO());
-                            stmt.setString(5, astr.getRangoMilitar());
-                            stmt.executeUpdate();
-
-                            ConectionDB.closeConexio(conn);
-                            return true;
-                        } catch (SQLException e) {
-                            System.out.println("error " + e.getMessage());
-                        }
-                    }
-                    break;
+                ConectionDB.closeConexio(conn);
+                return true;
+            } catch (SQLException e) {
+                System.out.println("error " + e.getMessage());
             }
         }
+        return false;
+    }
 
+    public static boolean addUsuario(Astronauta astr, String contrasena) throws SQLException {
+        ConectionDB conec = new ConectionDB();
+        Connection conn = conec.getConexio();
+
+        if (!userExists(astr)) {
+            try {
+                addToTableUsuarios(astr, "ASTR", contrasena);
+                PreparedStatement stmt = conn.prepareStatement("INSERT INTO astronauta"+
+                        "(astronauta_id, astrouauta_data_1r_vuelo, astronauta_misiones_ok, astronauta_misiones_ko, astronauta_rango_militar) "+
+                        "VALUES (?,?,?,?,?);");
+
+                stmt.setString(1, astr.getUsuario());
+                stmt.setDate(2, astr.getFechaPrimerVuelo());
+                stmt.setInt(3, astr.getMisionesOK());
+                stmt.setInt(4, astr.getMisionesKO());
+                stmt.setString(5, astr.getRangoMilitar());
+                stmt.executeUpdate();
+
+                ConectionDB.closeConexio(conn);
+                return true;
+            } catch (SQLException e) {
+                System.out.println("error " + e.getMessage());
+            }
+        }
         ConectionDB.closeConexio(conn);
+        return false;
+    }
+
+    private static boolean userExists(Usuario usuario) {
+        ConectionDB conec = new ConectionDB();
+        Connection conn = conec.getConexio();
+        try {
+            Statement sentencia = conn.createStatement();
+            ResultSet rs = sentencia.executeQuery("select usuario_usuario from usuarios where usuario_usuario = '" + usuario.getUsuario() + "';");
+
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            System.out.println("error " + e.getMessage());
+        }
         return false;
     }
 
@@ -128,6 +139,7 @@ public class Usuario {
             stmt.setString(7, usuario.getDireccion());
             stmt.executeUpdate();
 
+            ConectionDB.closeConexio(conn);
             return true;
         } catch (SQLException e) {
             System.out.println("error " + e.getMessage());
